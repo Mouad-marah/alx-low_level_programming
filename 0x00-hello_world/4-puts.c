424eeab270295d5c70316451fc7f06379ac3f799
Mouad-marah <mouadmarah0@gmail.com>
2023-06-15 10:40:18 +0000

    first C output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
+/* main - Entry point */
+ 
 int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
