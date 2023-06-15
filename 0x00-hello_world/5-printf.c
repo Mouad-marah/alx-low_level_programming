516b9cea6c547058af7eded77d07abca523b3837
Mouad-marah <mouadmarah0@gmail.com>
2023-06-15 10:58:20 +0000

    first printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*The power of printf function
+*
+*Return:0 (SUCCESS)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
