884466d17c532274a2a33df90d79abc4d6236250
Mouad-marah <mouadmarah0@gmail.com>
2023-06-16 11:51:06 -0700

    skip 2 chars

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Program print numbers from 0 to 9
+ *
+ * Return: 0 always SUCCESS
+*/
+
+int main(void)
+{
+int i = 0;
+while (i<10)
+{
+printf("%i", i);
+i++;
+}
+putchar('\n');
+return (0);
+}
