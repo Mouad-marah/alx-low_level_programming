284a92ebbf1284bf711f25ee62ebb5352d3da08f
Mouad-marah <mouadmarah0@gmail.com>
2023-06-16 17:47:25 -0700

    different combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription: combination of the 3 digits
+ *
+ * Return: 0 always SUCCESS
+*/
+
+int main(void)
+{
+	int i, Div, Mod, DivMod;
+
+for (i = 1; i < 1000; i++)
+{
+Div = i / 100;
+DivMod = (i / 10) % 10
+Mod = i % 10;
+	if (Div < DivMod && DivMod < Mod)
+	{
+		putchar(Div + '0');
+		putchar(DivMod + '0');
+		putchar(Mod + '0');
+	if (i < 789)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+}
+	putchar('\n');
+return (0);
+}
