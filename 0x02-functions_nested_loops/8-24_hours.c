787c239aa99e9f7aac35046a66387a1b078c8d83
Mouad-marah <mouadmarah0@gmail.com>
2023-06-20 13:56:13 -0700

    every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,7 +17,7 @@ void jack_bauer(void)
 		for (mins = 0; mins < 60; mins++)
 		{
 			_putchar((hours / 10) + '0');
-			_putchar((hours % 10)+ '0');
+			_putchar((hours % 10) + '0');
 			_putchar(':');
 			_putchar((mins / 10) + '0');
 			_putchar((mins % 10) + '0');
