43ea23905b7ff9ec150abed3fbefb77d32281369
Mouad-marah <mouadmarah0@gmail.com>
2023-06-20 11:25:18 -0700

    last digit of a number.

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -16,13 +16,12 @@ int print_last_digit(int n)
 	if (ligit <= 9)
 	{
 		ligit = -1 * ligit;
-		_putchar(ligit + '0');
+		_putchar((ligit) + '0');
 		return (ligit);
 	}
 	else
 	{
-		ligit = n % 10;
-		_putchar(ligit + '0');
+		_putchar((ligit) + '0');
 		return (ligit);
 	}
 }
