4099beb7d6551488d97ac4c82b87017e1b4d9a33
Mouad-marah <mouadmarah0@gmail.com>
2023-06-20 12:54:00 -0700

    every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * jack_bauer - check the function code
+ *
+ * Count minute of the day
+ *
+ * Return:  return no value
+ */
+
+void jack_bauer(void)
+{
+	int hour, min, sec02, sec01, i, j;
+
+	for (j = 0; j < 24; j++)
+	{
+		hour = j / 10;
+		min = j % 10;
+
+		for (i = j + 1; i < 60; i++)
+		{
+			sec02 = i / 10;
+			sec01 = i % 10;
+
+			if (hour < sec02 || (hour == sec02 && sec02 > min))
+			{
+				_putchar(hour + '0');
+				_putchar(min + '0');
+				_putchar(':');
+				_putchar(sec02 + '0');
+				_putchar(sec01 + '0');
+				_putchar('\n');
+			}
+		}
+	}
+}
