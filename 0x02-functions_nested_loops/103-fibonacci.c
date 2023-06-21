722c6afc7e39b77f81dccad916f5f24490bc9dcc
Mouad-marah <mouadmarah0@gmail.com>
2023-06-20 19:32:10 -0700

    febonacci lkhanez ez 3lik

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,8 +11,8 @@
 int main(void)
 {
 	int i = 0;
-	long int pre = 1;
-	long int curr = 2;
+	long int pre = 0;
+	long int curr = 1;
 	long int next;
 
 		while (i < 50)
@@ -23,7 +23,7 @@ int main(void)
 
 			printf("%ld", next);
 
-			if (i <= 48)
+			if (i < 49)
 			{
 				printf(", ");
 			}
