2926b6593d4ea3f7cd1081a99dfe06325d8cbdf0
Mouad-marah <mouadmarah0@gmail.com>
2023-06-22 10:25:17 -0700

    prints the numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code.
+ *
+ * Return: Always
+ */
+
+void print_numbers(void)
+{
+	char i ;
+
+	for (i = 48; i <= 57 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
