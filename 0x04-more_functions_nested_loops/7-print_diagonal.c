59db93de223abe54ecb8ee34fbbddbaf5668da1e
Mouad-marah <mouadmarah0@gmail.com>
2023-06-22 11:41:59 -0700

    draws a straight line in the terminal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,26 +1,28 @@
 #include "main.h"
 
 /**
- * print_line  - check the code.
+ * print_diagonal - check the code.
  *
  *@n: variable
  *
  * Return: Always
  */
 
-void print_line(int n)
+void print_diagonal(int n)
 {
 	int c, i;
 
 	for (c = 0; c < n; c++)
 	{
-		for (i = 0; i < c; i++)
+		for (i = 0; i < n; i++)
 		{
-				_putchar(32);
+			_putchar(32);
 		}
 			_putchar(92);
 			_putchar('\n');
 	}
 		if (n <= 0)
+		{
 			_putchar('\n');
+		}
 }
