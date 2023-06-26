c9267d76cba9485f1609bafcb6ee01f1d1eb06ac
Mouad-marah <mouadmarah0@gmail.com>
2023-06-26 06:40:54 -0700

    even numbers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * puts2 - function declaration
+ *
+ * @str: arrgument of function
+*/
+
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str % 2 == 0)
+			putchar(*str);
+			str++;
+	}
+putchar('\n');
+}
