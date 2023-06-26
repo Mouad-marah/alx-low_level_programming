b92aeac6439e45abb1a4915dc8944aa1b5b93554
Mouad-marah <mouadmarah0@gmail.com>
2023-06-26 00:25:12 -0700

    string, in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+/**
+ * rev_string - function declaration
+ *
+ * @s: arrgument of the function
+*/
 
 void rev_string(char *s)
 {
 	int length, mid, i, temp, end;
-	
+
 	length = sizeof(s) / sizeof(s[0]);
 	end = length;
-	mid = length /2;
+	mid = length / 2;
 
 	for (i = 0; i < mid; i++)
 	{
