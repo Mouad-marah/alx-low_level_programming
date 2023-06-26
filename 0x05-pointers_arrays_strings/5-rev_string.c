7868c70752eec9e6256756e605dd5382d7e8df29
Mouad-marah <mouadmarah0@gmail.com>
2023-06-26 00:21:13 -0700

    string, in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void rev_string(char *s)
+{
+	int length, mid, i, temp, end;
+	
+	length = sizeof(s)/sizeof(s[0]);
+	end = length;
+	mid = length /2;
+
+	for (i = 0; i < mid; i++)
+	{
+		temp = s[i];
+		s[i] = s[end - i];
+		s[end - i] = temp;
+	}
+}
