629218df6c3d9204f8c72fb7d9a881239482bfa8
Mouad-marah <mouadmarah0@gmail.com>
2023-06-26 11:55:31 -0700

    copies the string pointed

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,15 +5,19 @@
  * @src: arrgument
  *
  * @dest: arrgument
+ *
+ * Return: dest value
 */
+
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while(*(src + i) != '\0')
+	while (*(src + i) != '\0')
 	{
 		*(dest + i) = *(src + i);
 		i++;
 	}
+	*(dest + i) = *(src + i);
 	return (dest);
 }
