28e563c2a764bf2b3580a45185634a231b2980f9
Mouad-marah <mouadmarah0@gmail.com>
2023-06-25 22:14:28 -0700

    that prints a string, followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - function declaration
+ *
+ * @str: variable
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
