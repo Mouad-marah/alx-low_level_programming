c043371af3faa322e18a29501248165180e94c5f
Mouad-marah <mouadmarah0@gmail.com>
2023-06-26 09:03:35 -0700

    half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/***/
+
+void puts_half(char *str)
+{
+	int length, mid;
+
+	length = 0;
+	mid = 0;
+
+	while (*str != '\0')
+	{
+		length++;
+		str++;
+	}
+		if (length % 2 == 0)
+		{
+			mid = length / 2;
+		}
+		else
+		{
+			mid = (length - 1) /2;
+		}
+	str = str - length;
+		while (*(str + mid))
+		{
+			putchar(*(str + mid));
+			str++;
+		}
+	putchar('\n');
+}
