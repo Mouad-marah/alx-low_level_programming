da277ef3668ff434e8439e5170b58b6571152497
Mouad-marah <mouadmarah0@gmail.com>
2023-07-03 10:12:06 -0700

    function that sets the value of a pointer to a char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - function declaration
+ *
+ * @s: pointer variable
+ * @to: string
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int _putchar(char c);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif
