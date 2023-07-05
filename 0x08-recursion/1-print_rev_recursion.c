e5a813707d8e97b11e7e937f0fc612b98af45de4
Mouad-marah <mouadmarah0@gmail.com>
2023-07-05 10:27:43 -0700

    string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function declaration
+ *
+ * @s: argument if the function
+*/
+
+void  _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+	_print_rev_recursion(s + 1);
+	putchar(*s);
+	}
+}
