c2198cc6702752700113b65c272866666c24a904
Mouad-marah <mouadmarah0@gmail.com>
2023-07-05 17:25:43 -0700

    sqrt of number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * check - checks the function
+ *
+ * @a:argument
+ * @b:argument
+ *
+ * Return: value of square
+*/
+
+int find(int x, int y)
+{
+	if (x * x == y)
+		return (x);
+	if (x * x > y)
+		return (-1);
+return (find(x + 1, y));
+}
+
+/**
+ * _sqrt_recursion - function declaration
+ *
+ * @n: the squar root
+ *
+ * Return: value of squarRoot
+*/
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+return (find(1, n));
+}
