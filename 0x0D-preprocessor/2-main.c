885f6adec95cf40d271128793e4080674b07a3d0
Mouad-marah <mouadmarah0@gmail.com>
2023-07-17 15:59:02 -0700

    print the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription: display the current file
+ *
+ * Return: 0 always success
+*/
+
+int main(void)
+{
+	printf("%s", __FILE__);
+	return (0);
+}
