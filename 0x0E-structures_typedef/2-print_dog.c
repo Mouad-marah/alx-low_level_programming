8c32d01ba9bd8eb83d0fda7ba0550966fcd6d713
Mouad-marah <mouadmarah0@gmail.com>
2023-07-17 18:28:51 -0700

    initialize a variable

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - function declaration
+ *
+ * @d: pointer
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		(d->name) ? printf("Name: %s\n", d->name) : printf("Name: (nil)\n");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", (d->owner == NULL) ? "(nil)" : d->owner);
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,6 @@ typedef struct dog
 
 dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
 #endif
