62d328ea4c2e1adcc2841bda79d13050388fbafe
Mouad-marah <mouadmarah0@gmail.com>
2023-07-17 18:03:53 -0700

    new type struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,19 +9,15 @@
  * @owner: pointer char
 */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * dog_t - typedef struct for dog
-*/
-
-typedef struct dog dog_t;
+} dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
 #endif
