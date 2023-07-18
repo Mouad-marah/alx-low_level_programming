3bc4a06fbaa60cc093a96945f3af4d7c0749db8d
Mouad-marah <mouadmarah0@gmail.com>
2023-07-17 17:29:16 -0700

    new type struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define dog_H
+
+/**
+ * struct dog - Structure buddy
+ *
+ * @name: pointer char
+ * @age: float variable
+ * @owner: pointer char
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
