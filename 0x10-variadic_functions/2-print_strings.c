26d6115478b5ee85dba3d47857c45f78c3429f57
Mouad-marah <mouadmarah0@gmail.com>
2023-07-20 16:34:23 -0700

    function that prints strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,9 +18,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(args, char *);
-		printf("%s", str);
+		if (str != 0)
+		{
+			str = va_arg(args, char *);
+			printf("%s", str);
 
+		else
+			printf("(nil)");
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
