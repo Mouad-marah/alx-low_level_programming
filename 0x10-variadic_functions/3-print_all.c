c212897fbbb1a6cc5c6aaa611b1cfc01ce91b20e
Mouad-marah <mouadmarah0@gmail.com>
2023-07-20 19:29:11 -0700

    function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -33,7 +33,7 @@ void print_all(const char * const format, ...)
 			case 's':
 				str = va_arg(args, char *);
 				if (str == NULL)
-					str = "(nil)";
+					printf("(nil)");
 				printf("%s", str);
 				break;
 			default:
