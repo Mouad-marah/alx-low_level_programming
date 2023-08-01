26ab7ff6a1161ba090225e75c05c501d009f5a35
Mouad-marah <mouadmarah0@gmail.com>
2023-08-01 16:34:38 -0700

    insert node int  popop

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *in_node = *head;
 	listint_t *new_node;
-	unsigned int index, c;
+	unsigned int index, c = 0;
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
