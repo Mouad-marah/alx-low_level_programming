bc0304659f913eb88ae787f1d48c07b59b021b1b
Mouad-marah <mouadmarah0@gmail.com>
2023-08-01 16:33:05 -0700

    insert node int  popop

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - funcion declaration
+ *
+ * @head: pointer to the list
+ * @idx: position
+ * @n: data
+ *
+ * Return: NULL
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *in_node = *head;
+	listint_t *new_node;
+	unsigned int index, c;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (*head);
+	}
+	index = idx - 1;
+	while (in_node && c != index)
+	{
+		c++;
+		in_node = in_node->next;
+	}
+	if (c == index && in_node)
+	{
+		new_node->next = in_node->next;
+		in_node->next = new_node;
+		return (new_node);
+	}
+
+	free(new_node);
+	return (NULL);
+}
