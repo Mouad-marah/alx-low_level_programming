5a2cf118ddbcf00cbbd66b521be4bcaf7b479353
Mouad-marah <mouadmarah0@gmail.com>
2023-08-03 09:40:45 -0700

    bit s fun

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * binary_to_uint - function declaration
+ *
+ * @b: char arg
+ *
+ * Return: a SUCCESS
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int a;
+	int len, baseTwo;
+
+	a = 0;
+	len = 0;
+	if (!b)
+		return 0;
+	while (b[len] != '\0')
+		len++;
+	for (len--,baseTwo = 1; len >= 0; len--, baseTwo *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+		if (b[len] & 1)
+			a += baseTwo;
+	}
+	return (a);
+}
