908060eaf609deb1d8ec2c1ef161d9dda513d432
Mouad-marah <mouadmarah0@gmail.com>
2023-08-03 12:11:19 -0700

    what is this man

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * get_bit _ function declaration
+ *
+ * @n: int argument
+ * @index: index to check
+ *
+ * Return: n & 1 SUCCESS
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	if (n == 0 && index < 64)
+		return (0);
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+			return (n & 1);
+	}
+	return (-1);
+}
