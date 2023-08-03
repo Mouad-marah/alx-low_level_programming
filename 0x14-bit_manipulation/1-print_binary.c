4909e6388307b8111d1e19ae0914a8558a10e26e
Mouad-marah <mouadmarah0@gmail.com>
2023-08-03 12:03:17 -0700

    were did binary came from

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,12 +8,11 @@ void print_binary(unsigned long int n)
 {
 	if (n >> 0)
 	{
-		if (n >> 1)
-		{
+		if (n >> 1)		
 			print_binary(n >> 1);
-			putchar((n & 1) + '0');
-		}
+		putchar((n & 1) + '0');
+	}
 		else
 			putchar('0');
-	}
+
 }
