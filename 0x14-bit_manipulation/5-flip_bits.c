c1243682ad9ed27bd8eddffb61fb1e4505353f80
Mouad-marah <mouadmarah0@gmail.com>
2023-08-03 12:38:52 -0700

    lets flip everythings

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/***/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i;
+
+	for (i = 0; n || m; n >>= 1, m >>= 1)
+	{
+		if ((n & 1) != (m & 1))
+			i++;
+	}
+	return (i);
+}
